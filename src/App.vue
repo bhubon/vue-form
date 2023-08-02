661055c4cbfe2b3ed570e1c37dd38465f1281817
Bhubon Sutradhar Nil <54433969+bhubon@users.noreply.github.com>
2023-08-02 13:08:18 +0600

    Simple quize app with vue model binding

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { reactive } from 'vue';
+const saarcCountries = reactive([
+  { name: "Bangladesh", capital: "Dhaka" },
+  { name: "Nepal", capital: "Kathmandu" },
+  { name: "Bhutan", capital: "Thimphu" },
+  { name: "Sri Lanka", capital: "Colombo" },
+  { name: "India", capital: "New Delhi" },
+  { name: "Pakistan", capital: "Islamabad" },
+  { name: "Maldives", capital: "Male" }
+])
+
+const capitals = ["Dhaka", "Kathmandu", "Thimphu", "Colombo", "New Delhi", "Islamabad", "Male"]
+
+function getScore() {
+  let score = 0;
+  saarcCountries.forEach(country => {
+    if (country.answer == country.capital) {
+      score++
+    }
+  });
+  return score;
+}
+</script>
+
+<template>
+  <section class="mx-auto container text-left">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="mb-10">{{ saarcCountries }}</p>
+    <p>Your Score: {{ getScore() }}</p>
+    <div class="my-5 border border-gray-400 p-5" v-for="country in saarcCountries" :key="country.name">
+      <p>What is the capital of {{ country.name }}?</p>
+      <template v-for="capital in capitals">
+        <input type="radio" :value="capital" :name="country.name" v-model="country.answer" />
+        <label class="ml-2 mr-2">{{ capital }}</label>
+      </template>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
